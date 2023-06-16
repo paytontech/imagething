25045c20044376af5e47c8b4930be027f435640c
paytondev <81662879+paytontech@users.noreply.github.com>
2023-06-16 19:47:52 -0400

    more rprogeress

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 import * as fs from 'fs'
+import os from 'os'
 main()
-async function main() {
-   const files = await readFilesInDirectory('https://sourceforge.net/projects/sshwindows/files/latest/download')
+const imgRegex = new RegExp('\\.(jpg|jpeg|png|gif|bmp|svg)$', 'i');
+const fileRegex = new RegExp('.+\\..+$');
 
-   console.log(await files)
+async function main() {
+    let dirs = ['Desktop', 'Documents', 'Downloads', 'Music', 'Pictures', 'Videos']
+    // parseFiles(`${os.homedir()}\\Downloads`)
+    for (let dir of dirs) {
+        parseFiles(`${os.homedir()}\\${dir}`)
+    }
 }
 
-async function readFilesInDirectory(path) {
-    return await fs.readdir(path)
+function replace(path) {
+    console.log(path)
+}
+
+async function parseFiles(path) {
+    await fs.readdir(path, (err, files) => {
+        if (err != null) {
+            console.error(err)
+            return
+        }
+        let filesParsed = []
+        for (let file of files) {
+            if (imgRegex.test(file)) {
+                filesParsed.push(file)
+            }
+        }
+        for (let file of filesParsed) { replace(`${path}/${file}`) }
+        for (let file of files) {
+            const fspath = `${path}/${file}`
+            var isFile = false
+            fs.lstat(fspath, (er2, stats) => {
+                if (stats.isDirectory()) {
+                    parseFiles(`${path}/${file}`)
+                }
+            })
+        }
+    })
+
 }
